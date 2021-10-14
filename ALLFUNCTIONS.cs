a4ba18a3564c13f290104495c317faa1c29df488
MaliciousKey <89441166+MaliciousKey@users.noreply.github.com>
2021-10-13 22:15:54 -0400

    Update and rename Hello-World-EZCode.cs to ALLFUNCTIONS.cs

---
+++ ALLFUNCTIONS.cs
@@ -0,0 +1,41 @@
+//ALL FUNCTIONS IN ONE FUNCTION
+
+using System;
+using EZCode;
+using System.IO;
+					
+public class Program {
+	public static void Main() {
+	        //Console Methods
+		EZCode.Console.WriteLine("Hello World!");
+		EZCode.Console.WriteInt(5);
+		EZCode.Console.ReadLine();
+		EZCode.Console.ReadKey();
+		EZCode.Console.ClearConsole();
+		
+	        //Math Methods
+	        EZCode.Math.Add(int1,int2);
+		EZCode.Math.Subtract(int1,int2);
+		EZCode.Math.Multiply(int1,int2);
+		EZCode.Math.Divide(int1,int2);
+		EZCode.Math.Sin(int1);
+		EZCode.Math.Cos(int1);
+		EZCode.Math.Tan(int1);
+		EZCode.Math.Pow(int1);
+		EZCode.Math.Round(int1,int2);
+		
+		//Files Methods
+		EZCode.Files.Create("monkey.monke");
+		EZCode.Files.Delete("monkey.monke");
+		EZCode.Files.Move("monkey.monke",destination);
+		EZCode.Files.Copy("monkey.monke",destination);
+		EZCode.Files.Open("monkey.monke",FileMode.Open);
+		
+		//Loop Methods
+		EZCode.Loops.FLoopDoFunc(length,functionName);
+		EZCode.Loops.FLoopConsoleWriteLine(length,line);
+		EZCode.Loops.FLoopWriteInt(length,int1);
+		
+		//EZCode Specials Coming Soon!
+	}
+}
